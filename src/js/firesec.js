6a027f4566281b0c97f29558559db55a37cc5b29
Sudharsan <suddpro@gmail.com>
2020-04-23 20:50:32 +1200

    responsive design changes

---
+++ src/js/firesec.js
@@ -0,0 +1,5 @@
+import "../style/firesec.scss";
+import "../style/globalstyles.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+import "bootstrap/dist/js/bootstrap.js";
